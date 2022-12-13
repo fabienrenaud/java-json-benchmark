fed132d0c45cfb9dec72a46a3a12739629ec7c2d
Ethan McCue <ethan@mccue.dev>
2022-12-12 19:52:00 -0500

    Upgrade to JDK 17
    Update build.gradle

--- src/main/java/com/github/fabienrenaud/jjb/support/Libapi.java
+++ src/main/java/com/github/fabienrenaud/jjb/support/Libapi.java
@@ -4,52 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-public class Libapi {
-
-    private final boolean active;
-    private final Library lib;
-    private final List<Api> api;
-
+public record Libapi(boolean active, Library lib, List<Api> api) {
     public Libapi(Library lib, Api... api) {
         this(true, lib, api);
     }
 
     public Libapi(boolean active, Library lib, Api... api) {
-        this.active = active;
-        this.lib = lib;
-        this.api = Arrays.asList(api);
-    }
-
-    public boolean active() {
-        return active;
-    }
-
-    public Library lib() {
-        return lib;
-    }
-
-    public List<Api> api() {
-        return api;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Libapi)) {
-            return false;
-        }
-
-        Libapi libapi = (Libapi) o;
-
-        return active == libapi.active &&
-                lib == libapi.lib &&
-                Objects.equals(api, libapi.api);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(active, lib, api);
+        this(active, lib, Arrays.asList(api));
     }
 }
